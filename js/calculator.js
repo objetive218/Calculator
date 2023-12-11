ac82fd8dc2a4531733a8a61c412add061bf7171b
objetive218 <davidsantiagotc@gmail.com>
2023-12-10 22:05:47 -0500

    add clear, delete and addDot functions

--- js/calculator.js
+++ js/calculator.js
@@ -27,14 +27,30 @@ const clean = (e) => {
  })
 // del function 
 const deleting = () => {
-  if(res !== 0) {
-    let deleted = String(res).split("").pop();
+  if(numberOne != null && numberTwo === null) {
+    let delArray = String(numberOne).split("");
+    let deleted = delArray.splice(0, delArray.length -1)
+    numberOne = Number(deleted.join(""))
+    result.innerHTML = numberOne
+  }
+  else if (numberOne != null  && operation != null && numberTwo !=null && res === 0) {
+    let delArray = String(numberTwo).split("");
+    let deleted = delArray.splice(0, delArray.length -1)
+    numberTwo = Number(deleted.join(""))
+    result.innerHTML = numberTwo
+  }
+  else if (numberOne != null  && operation != null && numberTwo !=null && res != 0){
+    let delArray = String(res).split("");
+    let deleted = delArray.splice(0, delArray.length -1)
     res = Number(deleted.join(""))
+    result.innerHTML = res
   }
 }
 del.addEventListener("click", () => {
     deleting();
 })
+
+
 
 // declarations for oper function
 let numberOne = null;
@@ -47,7 +63,7 @@ let res = 0;
 const oper = (element) => {
   element.preventDefault();  
     if(numberOne === null && res === 0 && element.target.value !== "="){
-      console.log(typeof(element.target.value))
+      console.log(typeof(element.target.value), "add number 1")
       numberOne = res === 0 ? Number(element.target.value): res;
       result.innerHTML = numberOne;
     }
@@ -60,22 +76,22 @@ const oper = (element) => {
     else if(numberOne != null && operation === null && numberTwo === null && (element.target.value !== "/" 
     || element.target.value !== "-" || element.target.value !== "*" || element.target.value !== "+")&& element.target.value !== "=" ){
       let test= String(numberOne).split("").concat(element.target.value).join("");
-      console.log(test)
+      console.log(test,numberOne, "add number 1")
       numberOne = Number(test);
-      result.innerHTML = numberOne;
+      result.innerHTML = test;
     }
     else if(numberOne != null && operation !== null && numberTwo === null && (element.target.value !== "/" 
     || element.target.value !== "-" || element.target.value !== "*" || element.target.value !== "+") && element.target.value !== "="){
-      console.log(typeof(element.target.value))
+      console.log(typeof(element.target.value), "number 2")
       numberTwo =  Number(element.target.value);
       result.innerHTML = numberTwo;
     }
     else if(numberOne !== null && operation !== null && numberTwo !== null && (element.target.value !== "/" 
     || element.target.value !== "-" || element.target.value !== "*" || element.target.value !== "+") && element.target.value !== "=" ){
       let test1= String(numberTwo).split("").concat(element.target.value).join("");
-      console.log(test1)
+      console.log(test1 ,"add number 2")
       numberTwo = Number(test1);
-      console.log(numberTwo)
+      console.log(numberTwo,"add number 2")
       result.innerHTML = numberTwo;
     }       
     else if(numberOne != null && operation != null && numberTwo != null && element.target.value === "=" ){ 
@@ -119,6 +135,41 @@ const oper = (element) => {
               }
             }
 }
+
+
+//dot function 
+const addDot = (element) => {
+  element.preventDefault()
+  console.log("before")
+  let valDot = "."
+  let arrDot = [".", "  "];
+
+    if(numberOne != null && numberTwo === null){
+      //
+      let testNumber  = String(numberOne).split("").includes(".") ? numberOne: String(numberOne).split("").concat(valDot).join("");
+      numberOne = testNumber 
+      console.log(numberOne, testNumber, typeof(testNumber), "dot 1")
+      result.textContent = testNumber
+    }
+    else if(numberOne != null && numberTwo != null && operation != null){
+      //
+      let testNumber  = String(numberTwo).split("").includes(".") ? numberTwo :String(numberTwo).split("").concat(valDot).join("");
+      numberTwo = testNumber
+      console.log(numberTwo, testNumber, typeof(testNumber))
+      result.textContent = testNumber
+    }
+    else if(numberOne != null && numberTwo != null && operation != null && res !== 0 && preOp != null){
+      //
+      let testNumber  = String(res).split("").includes(".") ? res : String(res).split("").concat(valDot).join("");
+      res = testNumber
+      console.log(res, testNumber, typeof(testNumber))
+      result.textContent = testNumber
+    }
+  }
+  dot.addEventListener("click" , (e) => {
+    addDot(e);
+})
+
 result.innerHTML = res
 //const  all = document.querySelectorAll(".numbers");
 
